fac7d57a398e8701b649ae15fb15fc158ecb515b
leoidiaz <zaid.leonardo@gmail.com>
2020-01-06 10:40:56 -0800

    Initial Commit

---
+++ sideHustle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sideHustle
+//
+//  Created by Leonardo Diaz on 1/6/20.
+//  Copyright © 2020 Leonardo Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
